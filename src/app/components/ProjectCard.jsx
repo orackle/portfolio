c1f916f8a1a89462e5463bcad11e4fd595e71c8b
orackle <ranjana1997@gmail.com>
2024-02-21 02:46:58 -0500

    projects done

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { CodeBracketIcon, EyeIcon} from "@heroicons/react/24/outline"
+import Link from 'next/link'
 
-const ProjectCard = ({imgUrl, title, description}) => {
+const ProjectCard = ({imgUrl, title, description, gitUrl, previewUrl}) => {
     return (
         <div>
         <div className="h-52 md:h-72 rounded-t-xl relative group" style={{background: `url(${imgUrl})`, backgroundSize: "cover" }}>
             <div className="items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+            <Link className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link mr-2' href={gitUrl}>
+                    <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] cursor-pointer hover:white absolute top-1/2 left-1/2 tranform -translate-x-1/2 -translate-y-1/2 group-hover/link:text-white " />
+                    </Link>
+
+                    <Link className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link' href={previewUrl}>
+                    <EyeIcon className="h-10 w-10 text-[#ADB7BE] cursor-pointer hover:white absolute top-1/2 left-1/2 tranform -translate-x-1/2 -translate-y-1/2 group-hover/link:text-white" />
+                    </Link>
             </div>
             </div>
-            <div className="text-white rounded-b-xl bg-[#181818] py-6 pc-4">
+            <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
                 <h5 className='font-xl font-semibold mb-2'>{title}</h5>
                 <p className='text-[#ADB7BE] '>{description}</p>
 
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,7 +1,7 @@
 "use client"
-import React from 'react'
+import React, {useState }from 'react'
 import ProjectCard from './ProjectCard'
-
+import ProjectTag from './ProjectTag'
 
 const projects = [
     {
@@ -9,7 +9,9 @@ const projects = [
         title: "Hermes",
         description: "This is a project",
         imgUrl: "/images/projects/hermes.png",
-        tag: ["All", "Web","Mobile"]
+        tag: ["All", "Web","Mobile"],
+        gitUrl: "https://github.com/orackle/Hermes",
+        previewUrl: "https://github.com/orackle/Hermes/wiki"
     },
     {
 
@@ -17,33 +19,70 @@ const projects = [
         title: "eva (Edmonton Virtual Assistant)",
         description: "This is a project",
         imgUrl: "/images/projects/eva.png",
-        tag: ["All", "Web","Mobile"]
+        tag: ["All", "Web","Mobile"],
+        gitUrl: "/",
+        previewUrl: "/"
     },
     {
 
-        id: 3,
-        title: "Bon Voyage",
-        description: "Android Ride Sharing App with a payment system",
-        imgUrl: "/images/projects/bonvoyage.png",
-        tag: ["All", "Mobile"]
-    },
+      id: 3,
+      title: "Bon Voyage",
+      description: "Android Ride Sharing App with a payment system",
+      imgUrl: "/images/projects/bonvoyage.png",
+      tag: ["All", "Mobile"],
+      gitUrl: "https://github.com/orackle/bonvoyage",
+      previewUrl: "https://github.com/orackle/bonvoyage/wiki"
+  },
+  {
+
+    id: 4,
+    title: "Ada's Team Website",
+    description: "Website for Ada's Team - connecting university students with industry professionals",
+    imgUrl: "/images/projects/adas.png",
+    tag: ["All", "Web","Mobile"],
+    gitUrl: "https://github.com/adasdevelopers/adas-team-website",
+    previewUrl: "https://adasdevelopers.github.io/adas-team-website/"
+},
+{
+
+  id: 5,
+  title: "Edmonton Crime Report",
+  description: "Website for Ada's Team - connecting university students with industry professionals",
+  imgUrl: "/images/projects/epcd.png",
+  tag: ["All", "Web"],
+  gitUrl: "https://github.com/jha8/opendataproject",
+  previewUrl: "/"
+},
 ]
 const ProjectsSection = () => {
+    const [tag, setTag] = useState('All')
+    // function to filter out a project on tag
+
+    const handleTagChange = (newTag) =>{
+        setTag(newTag);
+    }
+    const filteredProjects = projects.filter((project) => project.tag.includes(tag));
     return (
         <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-      {/* <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-       
-      </div> */}
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+       <ProjectTag tag="All" isSelected={tag === "All"} onClick={handleTagChange} />
+
+       <ProjectTag tag="Web" isSelected={tag === "Web"} onClick={handleTagChange} />
+
+         <ProjectTag tag="Mobile" isSelected={tag === "Mobile"} onClick={handleTagChange} />    
+      </div>
       <ul className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {projects.map((project, index) => (
+        {filteredProjects.map((project) => (
             <ProjectCard
               key={project.id}
               title={project.title}
               description={project.description}
               imgUrl={project.imgUrl}
+                gitUrl={project.gitUrl}
+                previewUrl={project.previewUrl}
             />
         ))}
       </ul>
